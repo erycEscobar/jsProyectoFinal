c1b45a2c4c14e1c29149cbda6cc78f861d272a8a
Eric Escobar <eryc@live.com.ar>
2022-07-24 23:35:37 -0300

    Proyecto Final JS

--- JS/nav.js
+++ JS/nav.js
@@ -7,8 +7,8 @@ const createNav = () => {
         <img src="IMG/dark-logo.png" class="brand-logo" alt="">
         <div class="nav-items">
             <div class="search">
-                <input type="text" class="search-box" placeholder="search brand,product">
-                <button class="search-btn">search</button>
+                <input type="text" class="search-box" placeholder="marca, producto...">
+                <button class="search-btn">buscar</button>
             </div>
             <a href="#"><img src="IMG/user.png" alt=""></a>
             <a href="#"><img src="IMG/cart.png" alt=""></a>
@@ -16,9 +16,9 @@ const createNav = () => {
     </div>
     <ul class="links-container">
         <li class="link-item"><a href="#" class="link">home</a></li>
-        <li class="link-item"><a href="#" class="link">women</a></li>
-        <li class="link-item"><a href="#" class="link">men</a></li>
-        <li class="link-item"><a href="#" class="link">kids</a></li>
+        <li class="link-item"><a href="#" class="link">mujer</a></li>
+        <li class="link-item"><a href="#" class="link">hombre</a></li>
+        <li class="link-item"><a href="#" class="link">niño</a></li>
         <li class="link-item"><a href="#" class="link">accesorios</a></li>
     </ul>
     </nav>
